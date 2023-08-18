a934a597344a1888f56fdd658e17984147e4bfcb
StevenLiu <stevenliu986@gmail.com>
2023-08-18 20:31:44 +1000

    correct some mistakes

--- server/devBundle.js
+++ server/devBundle.js
@@ -1,11 +1,12 @@
 import webpack from "webpack";
+import webpackMiddleware from 'webpack-dev-middleware'
 import webpackHotMiddleware from "webpack-hot-middleware";
 import webpackConfig from "./../webpack.config.client.js";
 
 const compile = (app) => {
   if (process.env.NODE_ENV === "development") {
     const compiler = webpack(webpackConfig);
-    const middleware = webpackMIddleware(compiler, {
+    const middleware = webpackMiddleware(compiler, {
       publicPath: webpackConfig.output.publicPath,
     });
 
