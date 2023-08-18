94ac387c41b412adfef850bf0cd6722cf057840e
StevenLiu <stevenliu986@gmail.com>
2023-08-18 19:13:09 +1000

    add new lines

--- server/server.js
+++ server/server.js
@@ -1,5 +1,28 @@
 import express from "express";
+import { MongoClient } from "mongodb";
+import path from "path";
+import template from "../template";
 import devBundle from "./devBundle";
 
 const app = express();
 devBundle.compile(app);
+const CURRENT_WORKING_DIR = process.cwd();
+app.use("./dist", express.static(path.join(CURRENT_WORKING_DIR, "dist")));
+app.get("/", (req, res) => {
+  return res.status(200).send(template);
+});
+
+const url = process.env.MONGODB_URI || "mongodb://localhost:27017/test";
+MongoClient.connect(url, (err, db) => {
+  console.log("Connected successfully to mongodb server");
+  db.close();
+});
+
+let port = process.env.PORT || 3000;
+
+app.listen(port, function onStart(err) {
+  if (err) {
+    console.log(err);
+  }
+  console.info(`Server started on port ${port}.`);
+});
