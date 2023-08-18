cba6b9c6ef7fc2d7e6e9e2c6553e5d689ae699dd
StevenLiu <stevenliu986@gmail.com>
2023-08-18 21:14:00 +1000

    import module

--- client/HelloWorld.js
+++ client/HelloWorld.js
@@ -1,10 +1,11 @@
 import React from "react";
+import {hot} from "react-hot-loader"
 
 const HelloWorld = () => {
   return (
-    <>
+    <div>
       <h1>Hello World!</h1>
-    </>
+    </div>
   );
 };
 
